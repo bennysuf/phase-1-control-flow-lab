8294a6aba966cb23fb773f2bfa04913d8fb1ed3c
Benny Sufrin <bennysuf@gmail.com>
2022-08-09 14:01:52 -0400

    commit tree 958a0116330e0c4a282278ba125b3f7f5e497a64

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(n){
+  if (n <= 400){
+ return 'This one is on me!'
+  } else if (n > 400 && n < 2000) {
+    return 'That will be twenty bucks.'
+  } else if (n > 2000 && n < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
+};
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(c){
+ return c==='NYC'?'Ok, sounds good.':'No go.'
+};
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(x){
+  switch (x){
+    case 'generous': return 'Thank you so much.';
+    break;
+    case 'not as generous': return 'Thank you.';
+    break;
+    default: return 'Bye.'
+    break;
+  }
 }
